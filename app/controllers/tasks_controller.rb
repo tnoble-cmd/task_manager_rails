94eaa7bdac6d90a2db4003269b6a0d08a740b27e
Tyler Noble <tmnoble11@gmail.com>
2024-06-28 14:39:52 -0600

    final commit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,6 +19,27 @@ class TasksController < ApplicationController
   end
 
   def show
+    
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update({
+        title: params[:title],
+        description: params[:description]
+        })
+    task.save
+        
+    redirect_to "/tasks/#{task.id}"
+  end
+
+  def destroy
+    Task.destroy(params[:id])
+    redirect_to "/tasks"
   end
 end

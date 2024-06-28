bd57f8360444ee42e36545156628f0b4a8bc06bf
Tyler Noble <tmnoble11@gmail.com>
2024-06-28 13:22:45 -0600

    renamed: tasks.rb to task.rb. Modified: task_controller.rb index method to Task.all to display all tasks. Added DB model.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = ["Task 1", "Task 2", "Task 3"]
+    @tasks = Task.all
   end
   
   def new
@@ -8,13 +8,14 @@ class TasksController < ApplicationController
   end
 
   def create
+    binding.pry
     task = Task.new({
         title: params[:title],
         description: params[:description]
         })
-    
+    binding.pry
     task.save
-
+    binding.pry
     redirect_to "/tasks"
   end
 end

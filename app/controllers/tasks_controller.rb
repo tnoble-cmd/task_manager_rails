d996fbed71439efdaf9e662bd0adce3343b67e73
Tyler Noble <tmnoble11@gmail.com>
2024-06-28 13:33:07 -0600

    modified: Added to  task_controller show method. modified: index.html.erb changing h3 to href clickable link. modified: routes.rb to route for h3 href

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,14 +8,17 @@ class TasksController < ApplicationController
   end
 
   def create
-    binding.pry
     task = Task.new({
         title: params[:title],
         description: params[:description]
         })
-    binding.pry
+
     task.save
-    binding.pry
+
     redirect_to "/tasks"
+  end
+
+  def show
+    
   end
 end
